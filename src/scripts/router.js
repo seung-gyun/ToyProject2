e105c390c9ce334384d61138e047293e9bd0003a
seung-gyun <83224551+seung-gyun@users.noreply.github.com>
2024-06-22 23:37:58 +0900

    front commit
    익숙해진다 쫌

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,10 +1,14 @@
 import MainPage from '@/components/MainPage.vue'
-import LoginPage from '@/components/pages/LoginPage.vue'
+import LoginPage from '@/components/memberpage/LoginPage.vue'
+import FindPwdPage from '@/components/memberpage/FindPwdPage.vue'
+import JoinMemberPage from '@/components/memberpage/JoinMemberPage.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
     {path: '/', component:MainPage},
     {path: '/login', component:LoginPage},
+    {path: '/joinmember', component:JoinMemberPage},
+    {path: '/findpwd', component:FindPwdPage},
 
 ]
 
