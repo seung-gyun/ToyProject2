7d1b3515705e995e827e50712b1b9dfa1eef829d
seung-gyun <83224551+seung-gyun@users.noreply.github.com>
2024-06-26 21:13:13 +0900

    Spring Security Commit
    보완해야함

--- src/components/memberpage/LoginPage.vue
+++ src/components/memberpage/LoginPage.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="form-signin w-100 m-auto">
     <p class="mt-5 mb-3 text-body-secondary"> ※ Press Your Id  &  PassWord  </p>  
-    <div class="form-floating">       
-        <input type="text" class="form-control" id="memberId" name="memberId" ref="memberIdInput" v-model="state.members.memberId" @keyup.enter="submit()">
-
-        <label for="floatingInput">ID</label>
+        <form action="/savemoney/login" method="post" class="needs-validation" novalidate>
+      <div class="form-floating">
+        <input type="text" class="form-control" id="username" name="username" ref="memberIdInput" v-model="state.members.memberId" required>
+        <label for="memberId">ID</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="password" name="password" ref="passwordInput" v-model="state.members.password"
-        @keyup.enter="submit()">
-        <label for="floatingPassword">Password</label>
+        <input type="password" class="form-control" id="password" name="password" ref="passwordInput" v-model="state.members.password" required>
+        <label for="password">Password</label>
       </div>
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <!-- <button type="submit" class="btn btn-primary">Login</button> -->
+    </form>
       <div class="msg">
         {{ msgState.msg }}
       </div>
@@ -21,17 +23,16 @@
           <router-link to="/findPwdPage">비밀번호 찾기</router-link>&nbsp;&nbsp;
         </label>
       </div>
-      <button @click="submit()" class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
       
   </div>
   <hr class="my-4">
 </template>
 
 <script>
-import store from '@/scripts/store';
-import axios from 'axios';
+// import store from '@/scripts/store';
+// import axios from 'axios';
 import { reactive } from 'vue';
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router';
 
 
 // import axios from 'axios';
@@ -43,7 +44,7 @@ export default {
 
   setup(){
 
-    const router = useRouter();
+    // const router = useRouter();
 
     const state = reactive({
       members : {
@@ -58,40 +59,42 @@ export default {
       }
     })
    
-    const submit = ()=>{
+    // const submit = ()=>{
 
-      if (!state.members.memberId) {
-        alert('아이디를 입력하시오.');
-        return;
-      }
+    //   if (!state.members.memberId) {
+    //     alert('아이디를 입력하시오.');
+    //     return;
+    //   }
 
-      if (!state.members.password) {
-        alert('비밀번호를 입력하세요');
-        return;
-      }
+    //   if (!state.members.password) {
+    //     alert('비밀번호를 입력하세요');
+    //     return;
+    //   }
 
-      axios.post("/savemoney/login", state.members).then((response) => {
+    //   axios.post("/savemoney/login", state.members).then((response) => {
 
-        if(response.data.msg == null){ // 로그인 성공
+    //     alert("test");
 
-          //store값에 저장하겠다.
-          store.commit('setAccount', response.data.memberId);
-          sessionStorage.setItem("id",response.data.memberId);
+    //     if(response.data.msg == null){ // 로그인 성공
 
-           router.push('/');
+    //       //store값에 저장하겠다.
+    //       store.commit('setAccount', response.data.memberId);
+    //       sessionStorage.setItem("id",response.data.memberId);
+
+    //        router.push('/');
            
-        }
-        else{
-          msgState.msg = response.data.msg;
-        }
+    //     }
+    //     else{
+    //       msgState.msg = response.data.msg;
+    //     }
 
-      }).catch(error =>{
-        console.error("error", error);
-      })
+    //   }).catch(error =>{
+    //     console.error("error", error);
+    //   })
 
-    }
+    // }
 
-    return {state, submit, msgState}
+    return {state,/** submit,**/ msgState}
 
 
   }
