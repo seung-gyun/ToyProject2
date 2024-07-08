6cbc7aeadfca81fdd41a08ba3e7135b7ed620cc0
seung-gyun <83224551+seung-gyun@users.noreply.github.com>
2024-07-08 14:40:56 +0900

    commit
    front단에서 보안 강화

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -10,6 +10,7 @@ import NoticePage from '@/components/aboutnotice/NoticePage.vue'
 import InsertionNotice from '@/components/aboutnotice/InsertionNotice.vue'
 import DetailNotice from '@/components/aboutnotice/DetailNotice.vue'
 import {createRouter, createWebHistory} from 'vue-router'
+import store from './store'
 
 
 
@@ -24,13 +25,35 @@ const routes = [
     {path: '/noticepage', component:NoticePage, props: route => ({ currentPage : route.query.currentPage })},
     {path: '/insertionnotice', component:InsertionNotice},
     {path: '/detailnotice', component: DetailNotice, props: route => ({ boardid: route.query.boardid }) }
+
 ]
 
 const router = createRouter({
+
     history:createWebHistory(),
     routes
+
 })
 
+// 전역 가드 설정
+router.beforeEach((to, from, next) => {
+
+    const isAuthenticated = Boolean(store.state.account.memberId);
+    const AuthenticatedList = ['/mynotice', '/mypage'];
+    
+
+    if (AuthenticatedList.includes(to.path)) {
+        if (!isAuthenticated) {
+            // 인증되지 않은 경우 로그인 페이지로 리디렉션
+            alert("로그인이 필요한 페이지입니다.");
+            next({ path: '/login' });
+        } else {
+            next(); // 인증된 경우 이동 허용
+        }
+    } else {
+        next(); // 다른 경로는 모두 이동 허용
+    }
+});
 
 
 export default router
